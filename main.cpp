f7896ae7db7aa269e41e47f1ba2b7cb653e04c39
avilku <166584413+avilku@users.noreply.github.com>
2024-04-21 19:00:26 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 using namespace std;
-// to do list requirements
-// be able to add a task
-// delete a task
-// display all tasks
-// display options
 
 class ToDo {
     string task[10] = {""};
     int option = -1;
     int counter = 1;
+
 public:
     void options();
     void displayAll();
     void addTask();
     void deleteTask();
+    void sortTasks();
 };
+
 void ToDo::options() {
     while (option != 0) {
         cout << "--------To Do List--------" << endl;
         cout << "PRESS 1 TO ADD TASK" << endl;
         cout << "PRESS 2 TO DELETE TASK" << endl;
-        cout << "PRESS 3 TO SEE ALL TASKS" << endl;
+        cout << "PRESS 3 TO SORT TASKS" << endl;
+        cout << "PRESS 4 TO SEE ALL TASKS" << endl;
         cout << "PRESS 0 TO EXIT OUT OF PROGRAM" << endl;
         cout << "--------------------------" << endl;
-        cin >> option;
+    
+        cin >> option;    
         cin.ignore(); // Clear the newline character from the input buffer
         switch(option) {
             case 1:
@@ -34,25 +34,33 @@ void ToDo::options() {
                 deleteTask();
                 break;
             case 3:
+                sortTasks();
+                break;
+            case 4:
                 displayAll();
                 break;
+            // case 0:
+            //     break;
+            // default:
+            //     cout << "Invalid option. Try again." << endl;
         }
     }
 }
+
 void ToDo::displayAll(){
     cout << "Tasks To Be Completed" << endl;
     cout << "---------------------" << endl;
     for(int i = 1; i < counter; i++){
         cout << "Task " << i << " "<< task[i] << endl;
     }
-
 }
 
 void ToDo::addTask(){
     if(counter > 9){
-        cout <<"Task's are all full!" << endl;
+        cout <<"Tasks are all full!" << endl;
     }
     else{
+        cout << "Enter task: ";
         getline(cin, task[counter]);
         counter++;
     }
@@ -60,15 +68,32 @@ void ToDo::addTask(){
 
 void ToDo::deleteTask(){
     int deleteNumber = 0;
-    cout << "Enter the Number of the Task you Want to Delete" << endl;
+    cout << "Enter the Number of the Task you Want to Delete: ";
     cin >> deleteNumber;
-    for(int i = deleteNumber; i <counter-1; i++){
+    if (deleteNumber < 1 || deleteNumber >= counter) {
+        cout << "Invalid task number!" << endl;
+        return;
+    }
+    for(int i = deleteNumber; i < counter-1; i++){
         task[i] = task[i+1];
     }
     counter--;
 }
+
+void ToDo::sortTasks(){ //sort tasks in alphabetical order with bubble sort
+    for (int i = 1; i < counter; ++i) {
+        for (int j = 1; j < counter - i; ++j) {
+            if (task[j] > task[j + 1]) {
+                swap(task[j], task[j + 1]);
+            }
+        }
+    }
+}
+
 int main(){
     ToDo obj;
     obj.options();
     return 0;
 }
+
+//add option for invalid input later
