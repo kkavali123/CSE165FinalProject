26b06c8b078462b551cc49719a92e372afab89b8
avilku <166584413+avilku@users.noreply.github.com>
2024-04-22 22:30:00 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,9 @@
 #include <iostream>
 using namespace std;
-// Keerthi
-// to do list requirements
-// be able to add a task
-// delete a task
-// display all tasks
-// display options
-// another class called deadlines where you can put important dates
-// sort deadline by priority????
-// April 20 Keerthi - add another class Amrita - some kind of algorithm implementation
-// April 21 Keerthi - inheritcance/polymorphism and deriving objects from the abstract class + upcasting
 
-// potentially a seperate date class
-// now use a sorting algorithm to sort the display based on due date (Amrita)
-// gives the dates a restriction
+// 4/21 Amrita - accessor/mutators + properly destroy all memory + any initlaization stuff that i did wrong 
 
-
-// 4/21 Amrita - destructor for date class + accessor/mutators + properly destroy all memory + any initlaization stuff that i did wrong 
-// sort by deadlines, with earliest deadlines being first
-// invalid options in the to do list + invalid options for dates
-// optional Amrita - sort by farthest away deadline
-// 4/21 Keerthi - include polymmorphism/inheritance of some kind + abstract class + upcasting
+//getter/setter methods are accessors/mutators - ADD LATER after discussing in 4/23 meeting tomorrow
 
 class Date {
     string task;
@@ -32,18 +15,22 @@ public:
     int getDay() const { return day; }
     int getMonth() const { return month; }
     int getYear() const { return year; }
+    ~Date() {} 
 };
 class ToDo {
     Date task[10]; 
     int option = -1;
     int counter = 1;
 public:
+    ~ToDo() {} 
     void options();
     void displayAll();
     void addTask();
     void deleteTask();
+    void sortTasksAscending();
+    void sortTasksDescending();
     void filterTask();
-    void showBy(int month, int year); // Move the function declaration here
+    void showBy(int month, int year); 
 };
 
 void ToDo::showBy(int month, int year){
@@ -57,7 +44,7 @@ void ToDo::showBy(int month, int year){
         }
     }
     if(!match){
-        cout <<"There are no tasks due this month!" << endl;
+        cout << "There are no tasks due this month!" << endl;
     }
 }
 
@@ -66,8 +53,10 @@ void ToDo::options() {
         cout << "--------To Do List--------" << endl;
         cout << "PRESS 1 TO ADD TASK" << endl;
         cout << "PRESS 2 TO DELETE TASK" << endl;
-        cout << "PRESS 3 TO SEE ALL TASKS" << endl;
-        cout << "PRESS 4 TO FILTER TASKS BY MONTH" << endl;
+        cout << "PRESS 3 TO SORT TASKS IN ASCENDING ORDER" << endl;
+        cout << "PRESS 4 TO SORT TASKS IN DESCENDING ORDER" << endl;
+        cout << "PRESS 5 TO SEE ALL TASKS" << endl;
+        cout << "PRESS 6 TO FILTER TASKS BY MONTH" << endl;
         cout << "PRESS 0 TO EXIT OUT OF PROGRAM" << endl;
         cout << "--------------------------" << endl;
         cin >> option;
@@ -80,9 +69,15 @@ void ToDo::options() {
                 deleteTask();
                 break;
             case 3:
-                displayAll();
+                sortTasksAscending();
                 break;
             case 4:
+                sortTasksDescending();
+                break;
+            case 5:
+                displayAll();
+                break;
+            case 6:
                 filterTask();
                 break;
         }
@@ -98,9 +93,9 @@ void ToDo::displayAll(){
 
 }
 
-void ToDo::addTask(){
+void ToDo::addTask(){  //maybe add extra if statements for certain months/years for valid dates? check comments below (DISCUSS)
     if(counter > 9){
-        cout <<"Task's are all full!" << endl;
+        cout << "Task's are all full!" << endl;
     }
     else{
         string newTask;
@@ -109,10 +104,22 @@ void ToDo::addTask(){
         getline(cin, newTask);
         cout << "Enter the month: " << endl;
         cin >> month;
-        cout << "Enter the day: " << endl;
-        cin >> day;
+        if(month > 12 || month < 1){
+            cout << "Number not valid. Enter a valid number, please." << endl;
+            return;
+        }
+        cout << "Enter the day: " << endl;  //add option later to see that for certain months, certain numbers can't be used (ex. Feb, April)
+        cin >> day;         //think about leap year for February? (i.e. having the ability to only add 29 when it is a leap year? - discuss later)
+        if(day > 31 || day < 1){
+            cout << "Number not valid. Enter a valid number, please." << endl;
+            return;
+        }
         cout << "Enter the year: " << endl;
         cin >> year;
+        if(year < 0){
+            cout << "Number not valid. Enter a valid number, please." << endl;
+            return;
+        }
 
         task[counter] = Date(day, month, year, newTask);
         counter++;
@@ -120,14 +127,56 @@ void ToDo::addTask(){
     }
 }
 
-void ToDo::deleteTask(){
+void ToDo::deleteTask(){  //maybe add another aspect where you can delete multiple tasks in one command? (discuss tomorrow)     
     int deleteNumber = 0;
-    cout << "Enter the Number of the Task you Want to Delete" << endl;
+    cout << "Enter the number of the task you want to delete: ";
     cin >> deleteNumber;
-    for(int i = deleteNumber; i <counter-1; i++){
+    if (deleteNumber < 1 || deleteNumber >= counter) { 
+        cout << "Invalid task number! Try again." << endl; 
+        return;
+    }
+    for(int i = deleteNumber; i < counter-1; i++){
         task[i] = task[i+1];
     }
     counter--;
+}
+
+
+
+bool compareAscending(const Date& date1, const Date& date2) { 
+    if (date1.getYear() != date2.getYear())
+        return date1.getYear() > date2.getYear(); 
+    if (date1.getMonth() != date2.getMonth())
+        return date1.getMonth() > date2.getMonth(); 
+    return date1.getDay() > date2.getDay(); 
+}
+
+void ToDo::sortTasksAscending() {
+    for(int i = 1; i < counter; ++i) {
+        for(int j = 1; j < counter - i; ++j) {
+            if(compareAscending(task[j], task[j + 1])) {
+                swap(task[j], task[j + 1]);
+            }
+        }
+    }
+}
+
+bool compareDescending(const Date& date1, const Date& date2) { 
+    if (date1.getYear() != date2.getYear())
+        return date1.getYear() < date2.getYear(); 
+    if (date1.getMonth() != date2.getMonth())
+        return date1.getMonth() < date2.getMonth(); 
+    return date1.getDay() < date2.getDay(); 
+}
+
+void ToDo::sortTasksDescending(){
+    for(int i = 1; i < counter; ++i) {
+        for(int j = 1; j < counter - i; ++j) {
+            if(compareDescending(task[j], task[j + 1])) {
+                swap(task[j], task[j + 1]);
+            }
+        }
+    }
 }
 
 void ToDo::filterTask(){
