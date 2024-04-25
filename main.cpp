04ff905908f8d19af4430d5a800587f6fbefa677
kkavali123 <56940860+kkavali123@users.noreply.github.com>
2024-04-25 11:37:45 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 // notes + IMPORTANT
@@ -8,8 +10,18 @@ using namespace std;
 // STILL HAVE TO DO:
 // K - inheritance, abstract class, upcasting
 // A - polymorphism
+// looking into QT + having some code ready
 
-class Date {
+class Task{
+public:
+    virtual void display(int taskNumber) const = 0;
+    virtual ~Task() {}
+};
+// abstract base class = class designed to be used as a base class
+// contains at least one pure virtual function (=0)
+// can create an array of objects to save space 
+
+class Date : public Task {
     string task;
     int day, month, year;
 public: 
@@ -23,38 +35,58 @@ public:
     void setMonth(int m) { month = m; }
     int getYear() const { return year; }
     void setYear(int y) { year = y; }
-    ~Date() {} 
+    void display(int taskNumber) const override {
+        cout << "Task " << taskNumber << ": " << task << "(Due: " << month << "/" << day << "/" << year << ")" << endl;
+    }
+    // ~Date() {} 
 };
-class ToDo {
-    Date task[10]; 
+
+class ToDo : public Task{
+    vector<Task*> tasks;
     int option = -1;
-    int counter = 1;
 public:
-    ~ToDo() {} 
+    ~ToDo() {
+        for(auto task : tasks){
+            delete task;
+        }
+    } 
     void options();
-    void displayAll();
+    // void displayAll();
     void addTask();
     void deleteTask();
     void sortTasksAscending();
     void sortTasksDescending();
     void filterTask();
-    void showBy(int month, int year); 
+    void display(int taskNumber) const override;
+    // void showBy(int month, int year); 
 };
 
-void ToDo::showBy(int month, int year){
-    cout << "Tasks Due During " << month << "/" << year <<":" << endl;
-    cout << "------------------------" << endl;
-    bool match = false;
-    for(int i = 0; i < counter; i++){
-        if(task[i].getMonth() == month && task[i].getYear() == year){
-            match = true;
-            cout << "Tasks: " << task[i].getTask() << " (Due: " << task[i].getDay() << "/" << task[i].getMonth() << "/" << task[i].getYear() << ")" << endl;
-        }
+void ToDo::display(int taskNumber) const{
+    if(tasks.empty()){
+        cout << "No tasks to display!" << endl;
+        return;
     }
-    if(!match){
-        cout << "There are no tasks due this month!" << endl;
+    cout << "To-Do List: " << endl;
+    for(const auto& task : tasks){
+        task->display(taskNumber); 
+        // polymorphism
+        taskNumber++;
     }
 }
+// void ToDo::showBy(int month, int year){
+//     cout << "Tasks Due During " << month << "/" << year <<":" << endl;
+//     cout << "------------------------" << endl;
+//     bool match = false;
+//     for(int i = 0; i < counter; i++){
+//         if(task[i].getMonth() == month && task[i].getYear() == year){
+//             match = true;
+//             cout << "Tasks: " << task[i].getTask() << " (Due: " << task[i].getDay() << "/" << task[i].getMonth() << "/" << task[i].getYear() << ")" << endl;
+//         }
+//     }
+//     if(!match){
+//         cout << "There are no tasks due this month!" << endl;
+//     }
+// }
 
 void ToDo::options() {
     while (option != 0) {
@@ -82,7 +114,8 @@ void ToDo::options() {
                 sortTasksDescending();
                 break;
             case 5:
-                displayAll();
+                // displayAll();
+                display(1);
                 break;
             case 6:
                 filterTask();
@@ -90,21 +123,21 @@ void ToDo::options() {
         }
     }
 }
-void ToDo::displayAll(){
-    cout << "Tasks To Be Completed" << endl;
-    cout << "---------------------" << endl;
-    for(int i = 1; i < counter; i++){
-        Date currentTask = task[i];
-        cout << "Task " << i << " " << currentTask.getTask() << " (" << currentTask.getMonth() << "/" << currentTask.getDay() << "/" << currentTask.getYear() << ")" << endl;
-    }
+// void ToDo::displayAll(){
+//     cout << "Tasks To Be Completed" << endl;
+//     cout << "---------------------" << endl;
+//     for(int i = 1; i < counter; i++){
+//         Date currentTask = task[i];
+//         cout << "Task " << i << " " << currentTask.getTask() << " (" << currentTask.getMonth() << "/" << currentTask.getDay() << "/" << currentTask.getYear() << ")" << endl;
+//     }
 
-}
+// }
 
 void ToDo::addTask() {
-    if(counter > 9){
-        cout << "Tasks are all full!" << endl;
-    }
-    else {
+    // if(counter > 9){
+    //     cout << "Tasks are all full!" << endl;
+    // }
+    // else {
         string newTask;
         int day, month, year;
         cout << "Enter the task: " << endl;
@@ -142,61 +175,90 @@ void ToDo::addTask() {
             cout << "Not a valid date." << endl;
             return;
         }
+        tasks.push_back(new Date(day, month, year, newTask));
+        // upcasting
 
-        task[counter] = Date(day, month, year, newTask);
-        counter++;
-    }
+        // task[counter] = Date(day, month, year, newTask);
+        // counter++;
+    // }
 }
 
 
-void ToDo::deleteTask(){  //maybe add another aspect where you can delete multiple tasks in one command? (discuss tomorrow)     
+void ToDo::deleteTask(){  
     int deleteNumber = 0;
     cout << "Enter the number of the task you want to delete: ";
     cin >> deleteNumber;
-    if (deleteNumber < 1 || deleteNumber >= counter) { 
+    if (deleteNumber < 1 || deleteNumber > tasks.size()) { 
         cout << "Invalid task number! Try again." << endl; 
         return;
     }
-    for(int i = deleteNumber; i < counter-1; i++){
-        task[i] = task[i+1];
-    }
-    counter--;
+    // for(int i = deleteNumber; i < counter-1; i++){
+    //     task[i] = task[i+1];
+    // }
+    // counter--;
+    tasks.erase(tasks.begin() + (deleteNumber-1));
 }
 
 
-bool compareAscending(const Date& date1, const Date& date2) { 
-    if (date1.getYear() != date2.getYear())
-        return date1.getYear() > date2.getYear(); 
-    if (date1.getMonth() != date2.getMonth())
-        return date1.getMonth() > date2.getMonth(); 
-    return date1.getDay() > date2.getDay(); 
+
+// bool compareAscending(const Date& date1, const Date& date2) { 
+bool compareAscending(const Task* task1, const Task* task2){
+    const Date* date1 = dynamic_cast<const Date*>(task1);
+    const Date* date2 = dynamic_cast<const Date*>(task2);
+    if(date1 && date2){
+        if(date1->getYear() != date2->getYear()){
+            return date1->getYear() < date2->getYear();
+        }
+        if(date1->getMonth() != date2->getMonth()){
+            return date1->getMonth() < date2->getMonth();
+        return date1->getDay() < date2->getDay();
+        }
+        return false;
+    }
+    // if (date1.getYear() != date2.getYear())
+    //     return date1.getYear() > date2.getYear(); 
+    // if (date1.getMonth() != date2.getMonth())
+    //     return date1.getMonth() > date2.getMonth(); 
+    // return date1.getDay() > date2.getDay(); 
 }
 
 void ToDo::sortTasksAscending() {   
     cout << "Sorting tasks in ascending order..." << endl;
-    for(int i = 1; i < counter; ++i) {
-        for(int j = 1; j < counter - i; ++j) {
-            if(compareAscending(task[j], task[j + 1])) {
-                swap(task[j], task[j + 1]);
+    int n = tasks.size();
+    for(int i = 0; i < n-1; ++i) {
+        for(int j = 0; j < n - i - 1; ++j) {
+            if(!compareAscending(tasks[j], tasks[j + 1])) {
+                Task* temp = tasks[j];
+                tasks[j] = tasks[j+1];
+                tasks[j+1] = temp;
             }
         }
     }
 }
 
-bool compareDescending(const Date& date1, const Date& date2) { 
-    if (date1.getYear() != date2.getYear())
-        return date1.getYear() < date2.getYear(); 
-    if (date1.getMonth() != date2.getMonth())
-        return date1.getMonth() < date2.getMonth(); 
-    return date1.getDay() < date2.getDay(); 
+bool compareDescending(const Task* task1, const Task* task2){
+    const Date* date1 = dynamic_cast<const Date*>(task1);
+    const Date* date2 = dynamic_cast<const Date*>(task2);
+    if(date1 && date2){
+        if(date1->getYear() != date2->getYear()){
+            return date1->getYear() > date2->getYear();
+        }
+        if(date1->getMonth() != date2->getMonth()){
+            return date1->getMonth() > date2->getMonth();
+        return date1->getDay() > date2->getDay();
+        }
+        return false;
+    }
 }
-
 void ToDo::sortTasksDescending(){   
     cout << "Sorting tasks in descending order..." << endl;
-    for(int i = 1; i < counter; ++i) {
-        for(int j = 1; j < counter - i; ++j) {
-            if(compareDescending(task[j], task[j + 1])) {
-                swap(task[j], task[j + 1]);
+    int n = tasks.size();
+    for(int i = 0; i < n-1; ++i) {
+        for(int j = 0; j < n - i - 1; ++j) {
+            if(!compareDescending(tasks[j], tasks[j + 1])) {
+                Task* temp = tasks[j];
+                tasks[j] = tasks[j+1];
+                tasks[j+1] = temp;
             }
         }
     }
@@ -208,13 +270,29 @@ void ToDo::filterTask(){  //also cuts of first letter here (need to fix)
     cin >> month;
     if(month > 12 || month < 1){
         cout << "Enter a valid number, please." << endl;
+        return;
     }
     cout << "Enter the year you want to filter by: " << endl;
     cin >> year;
     if(year < 1){
         cout << "Enter a valid number, please." << endl;
+        return;
     }
-    showBy(month, year);
+    cout << "Tasks Due During " << month << "/" << year <<":" << endl;
+    cout << "------------------------" << endl;
+    bool match = false;
+    int taskNumber = 1;
+    for(const auto& task : tasks){
+        const Date* dateTask = dynamic_cast<const Date*>(task);
+        if(dateTask && dateTask->getMonth() == month && dateTask->getYear() == year){
+            match = true;
+            dateTask->display(taskNumber);
+            taskNumber++;
+        }
+    }
+    if(!match){
+        cout << "There are no tasks due this month!" << endl;
+    }
 }
 
 int main(){
